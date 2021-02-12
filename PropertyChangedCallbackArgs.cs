8ce0ea97886a088f5637a4eb3bfee2eb25ecda41
Yuuki-Walsh <yuuki.walsh@outlook.com>
2021-02-12 22:22:38 +1100

    initial version

---
+++ PropertyChangedCallbackArgs.cs
@@ -0,0 +1,67 @@
+﻿// © XIV-Tools.
+// Licensed under the MIT license.
+
+// MIT License
+//
+// Copyright (c) 2017 Marian Dolinský
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+namespace Wpf.Mv
+{
+	using System;
+	using System.Collections.Generic;
+	using System.ComponentModel;
+	using System.Runtime.CompilerServices;
+
+	/// <summary>
+	/// Callback data containing info about the changed property in the <see cref="PropertyChangedCallbackHandler"/>.
+	/// </summary>
+	public sealed class PropertyChangedCallbackArgs
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="PropertyChangedCallbackArgs"/> class.
+		/// </summary>
+		/// <param name="oldValue">Previous value of the changed property.</param>
+		/// <param name="newValue">Current value of the changed property.</param>
+		public PropertyChangedCallbackArgs(object oldValue, object newValue)
+		{
+			this.Handled = false;
+			this.OldValue = oldValue;
+			this.NewValue = newValue;
+		}
+
+		/// <summary>
+		/// Gets or sets a value indicating whether callback has been handled.
+		/// </summary>
+		public bool Handled { get; set; }
+
+		/// <summary>
+		/// Gets the previous value of the changed property.
+		/// </summary>
+		public object OldValue { get; }
+
+		/// <summary>
+		/// Gets the current value of the changed property.
+		/// </summary>
+		public object NewValue { get; }
+	}
+}
---
+++ View.cs
@@ -0,0 +1,68 @@
+﻿// © XIV-Tools.
+// Licensed under the MIT license.
+
+namespace Wpf.Mv
+{
+	using System;
+	using System.ComponentModel;
+	using System.Reflection;
+	using System.Runtime.CompilerServices;
+	using System.Windows.Controls;
+
+	public class View : UserControl, INotifyPropertyChanged
+	{
+		private readonly PropertyStore propertyStore;
+
+		protected View()
+		{
+			this.DataContext = this;
+			this.propertyStore = new PropertyStore();
+			this.propertyStore.PropertyChanged += this.OnPropertyStorePropertyChanged;
+
+			PropertyInfo[] properties = this.GetType().GetProperties();
+			foreach (PropertyInfo property in properties)
+			{
+				object defaultValue = null;
+
+				if (property.PropertyType.IsValueType)
+					defaultValue = Activator.CreateInstance(property.PropertyType);
+
+				this.propertyStore.RegisterProperty(property.Name, property.PropertyType, defaultValue);
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected bool IsPropertyChangedEventInvokingEnabled
+		{
+			get => this.propertyStore.IsPropertyChangedEventInvokingEnabled;
+			set => this.propertyStore.IsPropertyChangedEventInvokingEnabled = value;
+		}
+
+		protected bool IsPropertyChangedCallbackInvokingEnabled
+		{
+			get => this.propertyStore.IsPropertyChangedCallbackInvokingEnabled;
+			set => this.propertyStore.IsPropertyChangedCallbackInvokingEnabled = value;
+		}
+
+		protected object GetValue([CallerMemberName] string propertyName = null)
+		{
+			return this.propertyStore.GetValue(propertyName);
+		}
+
+		protected void ForceSetValue(object value, [CallerMemberName] string propertyName = null)
+		{
+			this.propertyStore.ForceSetValue(value, propertyName);
+		}
+
+		protected void SetValue(object value, [CallerMemberName] string propertyName = null)
+		{
+			this.propertyStore.SetValue(value, propertyName);
+		}
+
+		private void OnPropertyStorePropertyChanged(object sender, PropertyChangedEventArgs e)
+		{
+			this.PropertyChanged?.Invoke(this, e);
+		}
+	}
+}
