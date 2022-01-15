d4719114d618ab7bed062c86c92c8cbf9abfeba8
Yuki <yuuki.walsh@outlook.com>
2022-01-15 15:30:41 +1100

    added MultiBoolAndToVisibilityConverter

---
+++ Converters/MultiBoolAndToVisibilityConverter.cs
@@ -0,0 +1,34 @@
+﻿// © XIV-Tools.
+// Licensed under the MIT license.
+
+namespace XivToolsWpf.Converters
+{
+	using System;
+	using System.Globalization;
+	using System.Windows;
+	using System.Windows.Data;
+
+	public class MultiBoolAndToVisibilityConverter : IMultiValueConverter
+	{
+		public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+		{
+			foreach (object value in values)
+			{
+				if (value is bool boolValue)
+				{
+					if (!boolValue)
+					{
+						return Visibility.Collapsed;
+					}
+				}
+			}
+
+			return Visibility.Visible;
+		}
+
+		public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+		{
+			throw new NotSupportedException("BooleanAndConverter is a OneWay converter.");
+		}
+	}
+}
