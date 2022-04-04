b2cbbafe7e353e176b77f6b5f3909daf6ab805d9
Yuki <yuuki.walsh@outlook.com>
2022-04-04 21:22:24 +1000

    Add P/Invoke GetForegroundWindow to /really/ know if a window is active or not.

---
+++ Windows/WindowExtensions.cs
@@ -0,0 +1,24 @@
+﻿// © XIV-Tools.
+// Licensed under the MIT license.
+
+namespace XivToolsWpf.Windows
+{
+	using System;
+	using System.Runtime.InteropServices;
+	using System.Windows;
+	using System.Windows.Interop;
+
+	public static class WindowExtensions
+	{
+		[DllImport("user32.dll")]
+		private static extern IntPtr GetForegroundWindow();
+
+		private static bool GetIsActive(this Window? self)
+		{
+			if (self == null)
+				return false;
+
+			return GetForegroundWindow() == new WindowInteropHelper(self).Handle;
+		}
+	}
+}
