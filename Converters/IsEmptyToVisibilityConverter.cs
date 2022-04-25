fdfdddc0210b4dc725b5f3aa9407adf76ee2e97f
Yuki <yuuki.walsh@outlook.com>
2022-04-25 15:10:04 +1000

    IsEmptyToVisibilityConverter

---
+++ Converters/IsEmptyToVisibilityConverter.cs
@@ -0,0 +1,36 @@
+﻿// © XIV-Tools.
+// Licensed under the MIT license.
+
+namespace XivToolsWpf.Converters
+{
+	using System;
+	using System.Collections;
+	using System.Globalization;
+	using System.Windows;
+	using System.Windows.Data;
+
+	[ValueConversion(typeof(IEnumerable), typeof(Visibility))]
+	public class NotEmptyToVisibilityConverter : IValueConverter
+	{
+		public object Convert(object? value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value == null)
+				return Visibility.Visible;
+
+			if (value is IEnumerable enumerable)
+			{
+				foreach (object obj in enumerable)
+				{
+					return Visibility.Collapsed;
+				}
+			}
+
+			return Visibility.Visible;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
