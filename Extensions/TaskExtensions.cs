3c51dc939f6b96c7a364dcc15b39672ce01bb460
Yuki <yuuki.walsh@outlook.com>
2022-05-06 11:55:54 +1000

    Create TaskExtensions.cs

---
+++ Extensions/TaskExtensions.cs
@@ -0,0 +1,16 @@
+﻿// © XIV-Tools.
+// Licensed under the MIT license.
+
+namespace XivToolsWpf.Extensions;
+using System.Threading.Tasks;
+
+public static class TaskExtensions
+{
+	public static void Run(this Task self)
+	{
+		Task.Run(async () =>
+		{
+			await self;
+		});
+	}
+}
