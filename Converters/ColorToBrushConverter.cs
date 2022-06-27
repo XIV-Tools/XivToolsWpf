7432789d32c9adaac73e3729f063ff7de4a9d6ce
Yuki <yuuki.walsh@outlook.com>
2022-06-27 18:45:15 +1000

    added ColorToBrush converter

---
+++ Converters/ColorToBrushConverter.cs
@@ -0,0 +1,26 @@
+﻿// © XIV-Tools.
+// Licensed under the MIT license.
+
+namespace XivToolsWpf.Converters;
+
+using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+[ValueConversion(typeof(Color), typeof(Brush))]
+public class ColorToBrushConverter : IValueConverter
+{
+	public object Convert(object? value, Type targetType, object parameter, CultureInfo culture)
+	{
+		if (value is not Color color)
+			return new SolidColorBrush(Colors.Black);
+
+		return new SolidColorBrush(color);
+	}
+
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		throw new NotImplementedException();
+	}
+}
