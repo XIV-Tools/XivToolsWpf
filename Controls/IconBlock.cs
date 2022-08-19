c762fb10c50ed5e614e32921c4c7e3028fda9d94
Yuki <yuuki.walsh@outlook.com>
2022-08-19 21:58:00 +1000

    font awesome

--- Controls/IconBlock.cs
+++ Controls/IconBlock.cs
@@ -3,23 +3,45 @@
 
 namespace XivToolsWpf.Controls;
 
+using System.Collections.Generic;
+using System.Reflection;
 using System.Windows;
 using System.Windows.Media;
 using FontAwesome.Sharp;
 
 public class IconBlock : IconBlockBase<IconChar>
 {
-	private static readonly Typeface[] Typefaces = typeof(IconHelper).Assembly.LoadTypefaces("fonts", "Font Awesome 5 Free Solid");
+	private static readonly FontFamily NoneFont = new();
+	private static Typeface[]? typefaces;
 
 	public IconBlock()
-		: base(Font)
+		: base(NoneFont)
 	{
 	}
 
-	private static FontFamily Font => Typefaces[0].FontFamily;
-
 	protected override FontFamily FontFor(IconChar icon)
 	{
-		return Font;
+		if (typefaces == null)
+			Load();
+
+		GlyphTypeface gt;
+		ushort glyphIndex;
+		FontFamily? font = typefaces.Find(icon, out gt, out glyphIndex)?.FontFamily;
+
+		if (font == null)
+			return typefaces![0].FontFamily;
+
+		return font;
+	}
+
+	private static void Load()
+	{
+		string path = "fonts";
+
+		string[] names = new string[2];
+		names[0] = "Font Awesome 5 Pro Solid";
+		names[1] = "Font Awesome 5 Brands Regular";
+
+		typefaces = typeof(IconBlock).Assembly.LoadTypefaces(path, names);
 	}
 }
