39d6fb4dddbab00bab3e97a2475bc6f838229731
Yuki <yuuki.walsh@outlook.com>
2022-09-03 23:38:37 +1000

    Create SimpleCommand.cs

---
+++ Commands/SimpleCommand.cs
@@ -0,0 +1,35 @@
+﻿// © XIV-Tools.
+// Licensed under the MIT license.
+
+namespace XivToolsWpf.Commands;
+
+using System;
+using System.Windows.Input;
+
+public class SimpleCommand : ICommand
+{
+	private readonly Action action;
+	private readonly Func<bool> canExecute;
+
+	public SimpleCommand(Action action, Func<bool> canExecute)
+	{
+		this.action = action;
+		this.canExecute = canExecute;
+	}
+
+	public event EventHandler? CanExecuteChanged
+	{
+		add { CommandManager.RequerySuggested += value; }
+		remove { CommandManager.RequerySuggested -= value; }
+	}
+
+	public bool CanExecute(object? parameter)
+	{
+		return this.canExecute.Invoke();
+	}
+
+	public void Execute(object? parameter)
+	{
+		this.action.Invoke();
+	}
+}
