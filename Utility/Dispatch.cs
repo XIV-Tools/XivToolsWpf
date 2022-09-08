53bd7ad192983482e1348a1d2e0b23b6f6bc8593
Yuki <yuuki.walsh@outlook.com>
2022-09-08 20:13:51 +1000

    better dispatcher threading support

--- Utility/Dispatch.cs
+++ Utility/Dispatch.cs
@@ -5,43 +5,56 @@ namespace XivToolsWpf;
 
 using System;
 using System.Runtime.CompilerServices;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 
 public static class Dispatch
 {
-	public static SwitchToUiAwaitable MainThread()
-	{
-		return default(SwitchToUiAwaitable);
-	}
+	////public static SwitchToUiAwaitable MainThread() => new();
+	public static SwitchFromUiAwaitable NonUiThread() => new();
 
-	public static SwitchFromUiAwaitable NonUiThread()
-	{
-		return default(SwitchFromUiAwaitable);
-	}
+	public static SwitchToMainThreadAwaitable MainThread(this DispatcherObject self) => new(self.Dispatcher);
+	public static SwitchToMainThreadAwaitable MainThread(this Dispatcher self) => new(self);
 
-	public struct SwitchToUiAwaitable : INotifyCompletion
+	public struct SwitchToMainThreadAwaitable : INotifyCompletion
 	{
-		public bool IsCompleted => Application.Current?.Dispatcher.CheckAccess() == true;
+		private readonly Dispatcher dispatch;
 
-		public SwitchToUiAwaitable GetAwaiter()
+		public SwitchToMainThreadAwaitable(Dispatcher dispatcher)
 		{
-			return this;
+			this.dispatch = dispatcher;
 		}
 
+		public bool IsCompleted => this.dispatch?.CheckAccess() == true;
+
+		public SwitchToMainThreadAwaitable GetAwaiter() => this;
 		public void GetResult()
 		{
 		}
 
 		public void OnCompleted(Action continuation)
 		{
-			Application.Current?.Dispatcher.BeginInvoke(continuation);
+			this.dispatch.BeginInvoke(continuation);
 		}
 	}
 
 	public struct SwitchFromUiAwaitable : INotifyCompletion
 	{
-		public bool IsCompleted => Application.Current?.Dispatcher.CheckAccess() == false;
+		private readonly Dispatcher dispatch;
+
+		public SwitchFromUiAwaitable()
+		{
+			this.dispatch = Dispatcher.FromThread(Thread.CurrentThread);
+
+			if (this.dispatch == null)
+			{
+				this.dispatch = Application.Current.Dispatcher;
+			}
+		}
+
+		public bool IsCompleted => this.dispatch?.CheckAccess() == false;
 
 		public SwitchFromUiAwaitable GetAwaiter()
 		{
