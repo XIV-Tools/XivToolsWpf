3013011d74e276b1fa3a4c3a92360b65ef4f9fec
Yuki <yuuki.walsh@outlook.com>
2023-03-20 00:45:28 +1100

    Support for custom themes

---
+++ Extensions/ResourceDictionaryExtensions.cs
@@ -0,0 +1,21 @@
+﻿// © XIV-Tools.
+// Licensed under the MIT license.
+
+namespace XivToolsWpf.Extensions;
+
+using System.Windows;
+
+public static class ResourceDictionaryExtensions
+{
+	public static void Set(this ResourceDictionary self, string key, object value)
+	{
+		if (!self.Contains(key))
+		{
+			self.Add(key, value);
+		}
+		else
+		{
+			self[key] = value;
+		}
+	}
+}
---
+++ Themes/Themes.cs
@@ -0,0 +1,28 @@
+﻿// © XIV-Tools.
+// Licensed under the MIT license.
+
+namespace XivToolsWpf.Themes;
+
+using System.Windows;
+using System.Windows.Media;
+using XivToolsWpf.Extensions;
+
+public class CustomTheme
+{
+	public Color ForegroundBrush { get; set; } = Colors.White;
+	public Color ForegroundLightBrush { get; set; } = Colors.White;
+	public Color BackgroundBrush { get; set; } = Colors.White;
+	public Color BackgroundLightBrush { get; set; } = Colors.White;
+	public Color TrimBrush { get; set; } = Colors.White;
+	public Color ControlBackgroundBrush { get; set; } = Colors.White;
+
+	public void Apply(Application app)
+	{
+		app.Resources.Set("ForegroundBrush", new SolidColorBrush(this.ForegroundBrush));
+		app.Resources.Set("ForegroundLightBrush", new SolidColorBrush(this.ForegroundLightBrush));
+		app.Resources.Set("BackgroundBrush", new SolidColorBrush(this.BackgroundBrush));
+		app.Resources.Set("BackgroundLightBrush", new SolidColorBrush(this.BackgroundLightBrush));
+		app.Resources.Set("TrimBrush", new SolidColorBrush(this.TrimBrush));
+		app.Resources.Set("ControlBackgroundBrush", new SolidColorBrush(this.ControlBackgroundBrush));
+	}
+}
