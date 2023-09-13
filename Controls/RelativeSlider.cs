3ae5581640e192badffc46234236134a31b53003
Yuki <yuuki.walsh@outlook.com>
2023-09-13 18:49:52 +1000

    new Relative Slider control

---
+++ Controls/RelativeSlider.cs
@@ -0,0 +1,60 @@
+﻿// © XIV-Tools.
+// Licensed under the MIT license.
+
+namespace XivToolsWpf.Controls;
+
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using XivToolsWpf.DependencyProperties;
+
+public class RelativeSlider : Slider
+{
+	public static readonly IBind<double> RelativeValueDp = Binder.Register<double, RelativeSlider>(nameof(RelativeValue));
+	public static readonly IBind<double> RelativeRangeDp = Binder.Register<double, RelativeSlider>(nameof(RelativeRange), OnRelativeRangeChanged, BindMode.OneWay);
+
+	private double relativeSliderStart;
+
+	public RelativeSlider()
+	{
+		this.PreviewMouseDown += this.OnPreviewMouseDown;
+		this.PreviewMouseUp += this.OnPreviewMouseUp;
+		this.ValueChanged += this.OnValueChanged;
+
+		this.Value = 0;
+	}
+
+	public double RelativeValue
+	{
+		get => RelativeValueDp.Get(this);
+		set => RelativeValueDp.Set(this, value);
+	}
+
+	public double RelativeRange
+	{
+		get => RelativeRangeDp.Get(this);
+		set => RelativeRangeDp.Set(this, value);
+	}
+
+	private static void OnRelativeRangeChanged(RelativeSlider sender, double value)
+	{
+		sender.Minimum = -value;
+		sender.Maximum = value;
+	}
+
+	private void OnValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+	{
+		this.RelativeValue = this.relativeSliderStart + this.Value;
+	}
+
+	private void OnPreviewMouseDown(object? sender, MouseButtonEventArgs e)
+	{
+		this.relativeSliderStart = this.RelativeValue;
+	}
+
+	private void OnPreviewMouseUp(object? sender, MouseButtonEventArgs e)
+	{
+		this.relativeSliderStart = this.RelativeValue;
+		this.Value = 0;
+	}
+}
